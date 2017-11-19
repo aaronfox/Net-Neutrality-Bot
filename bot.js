cf4356c13b4897407118a44159771e24a37ca4f1
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2017-11-19 17:11:39 -0500

    Formatted and added more default text

--- bot.js
+++ bot.js
@@ -1,13 +1,9 @@
 var Twit = require('twit');
 var config = require('./config.js');
-
 var Twitter = new Twit(config);
-//=========
 var express = require('express');
 var app     = express();
-
 app.set('port', (process.env.PORT || 5000));
-
 //For avoiding Heroku $PORT error
 app.get('/', function(request, response) {
     var result = 'App is running'
@@ -15,14 +11,11 @@ app.get('/', function(request, response) {
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
-
 function tweetStatus(msg) {
     var tweet = {
         status: msg
     }
-    
     Twitter.post('statuses/update', tweet, tweeted);
-    
     function tweeted(err, data, res) {
         if (err) {
             console.log("Oh noes! Here's the error, champ: ");
@@ -33,7 +26,6 @@ function tweetStatus(msg) {
         };
     }
 }
-
 // THE FOLLOWING  GOES HERE
 function follow(screen_name) {
       Twitter.post('friendships/create', {screen_name}, function(err, response){
@@ -44,20 +36,15 @@ function follow(screen_name) {
         }
       });
 };
-
 //follow('BennieGThompson');
 //tweetStatus("I really love puppies, but net neutrality is more important right now!");
 
 //=============
-//T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-//  console.log(data)
-//});
-
 var retweet = function() {
   var params = {
     q: '#Puppy, #Dog',
     result_type: 'recent',
-    lang: 'en'    
+    lang: 'en'
   };
     Twitter.get('search/tweets', params, function(err, data) {
       // if there no errors
@@ -84,28 +71,22 @@ var retweet = function() {
         }
     });
 };
-
 // grab & retweet as soon as program is running...
 //retweet();
 // retweet in every 50 minutes
 //setInterval(retweet, 30000);
-
 var fs = require('fs-extra');
-
 // Follows people based on CSV file
 function csvHandler(){
   fs.readFile('115th-Congress-House-seeds.csv', function (err,data) {
-
   if (err) {
     return console.log(err);
   }
-
-  //Convert and store csv information into a buffer. 
-  bufferString = data.toString(); 
-
-  //Store information for each individual person in an array index. Split it by every newline in the csv file. 
-  arr = bufferString.split('\n'); 
-//  console.log(arr); 
+  //Convert and store csv information into a buffer.
+  bufferString = data.toString();
+  //Store information for each individual person in an array index. Split it by every newline in the csv file.
+  arr = bufferString.split('\n');
+//  console.log(arr);
       for (var i = 3; i < arr.length; i++)
           {
               console.log('tryna follow ' + arr[i].substr(0, arr[i].length - 1));
@@ -113,9 +94,7 @@ function csvHandler(){
           }
 });
 };
-
 //csvHandler();
-
 // Key = Twitter ID, Value = boolean of replied to last tweet
 var map = new Map();
 //var idArray;
@@ -124,21 +103,27 @@ var map = new Map();
 //    console.log(idArray);
 //});
 //console.log('idArray == ' + idArray[0]);
-
 var tweetIdsToReply;
 // Now, get latest tweets from people we follow
-
 var notifyPolitician = function() {
     Twitter.get('statuses/home_timeline', {count: 5, exclude_replies: true}, function(err, data, res) {
         for (var i = 0; i < data.length; i++)
             {
                 if(data[i].user.screen_name != 'NedNuTrality')
                     {
-    //                    console.log(data[i].id + ' ' + data[i].text);
+                        //console.log(data[i].id + ' ' + data[i].text);
                         // REPLY TO POLITICIAN HERE
-                        var hellos = ['Hi', 'Hello', 'Good day'];
-                        var greetings = ['Could I talk to you about net neutrality today? It\'s really important to the future of the US.', 'Net Neutrality is one of the most important principles keeping the Internet alive in the US today.', 'I would like to talk about net neutrality and how it\'s vitally important to the US.'];
-                        Twitter.post('statuses/update', {status: '@' + data[i].user.screen_name + ' ' + hellos[Math.floor(Math.random()*hellos.length)] + ', ' + greetings[Math.floor(Math.random()*greetings.length)] + ' Please support these regulations to ensure our Internet is safe and open to all. Please see @fightfortheftr for more info.', in_reply_to_status_id: data[i].id}, function(err, res) {
+                        var hellos = ['Hi',
+                                      'Hello',
+                                      'Good day'];
+                        var greetings = ['Could I talk to you about net neutrality today? It\'s really important to the future of the US.',
+                                        'Net Neutrality is one of the most important principles keeping the Internet alive in the US today.',
+                                        'I would like to talk about net neutrality and how it\'s vitally important to the US.'];
+                        var support = ['Please support these regulations to ensure our Internet is safe and open to all. Please see @fightfortheftr for more info.',
+                                      'Please take the time to support these regulations and make sure the internet remains awesome. Please see @fightfortheftr for more info.',
+                                      'Please help ensure that next generation has a free and safe internet. Please see @fightfortheftr for more info.'];
+                        var displayText = hellos[Math.floor(Math.random()*hellos.length)] + ', ' + greetings[Math.floor(Math.random()*greetings.length)] + ' ' + support[Math.floor(Math.random()*support.length)] ;
+                        Twitter.post('statuses/update', {status: '@' + data[i].user.screen_name + ' ' + displayText, in_reply_to_status_id: data[i].id}, function(err, res) {
                             if (err)
                                 {
                                     console.log("Error in replying to politician:");
@@ -149,10 +134,8 @@ var notifyPolitician = function() {
                             }
                         } );
                     }
-
             }
     });
 }
-notifyPolitician();
-
+//notifyPolitician();
 setInterval(notifyPolitician, 1000*60*30);
