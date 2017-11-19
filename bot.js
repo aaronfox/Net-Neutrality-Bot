13594e6c7d9ff8dd26a536d39f36d10269d69a6f
aaronfox <aaron.m.fox3@gmail.com>
2017-11-19 00:27:31 -0500

    posting a status now

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var app     = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//For avoidong Heroku $PORT error
+//For avoiding Heroku $PORT error
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.send(result);
@@ -16,8 +16,25 @@ app.get('/', function(request, response) {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
 
+function tweetStatus(msg) {
+    var tweet = {
+        status: msg;
+    }
+    
+    Twitter.post('statuses/update', tweet, tweeted);
+    
+    function tweeted(err, data, res) {
+        if (err) {
+            console.log("Oh noes! Here's the error, champ: ");
+            console.log(err);
+        }
+        else {
+            console.log("Success! Tweeted that awful message of yours.");
+        };
+    }
+}
 
-
+tweetStatus("I really love puppies, but net neutrality is more important right now!");
 
 //=============
 //T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
